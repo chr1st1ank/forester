5782970b8d83e7f307861a1fb6cf1ff3778efa49
chr1st1ank <chr1st1ank@krudewig-online.de>
2019-12-22 01:19:57 +0100

    Cleaned up pipenv, added pyenv/pipenv instructions to README

--- command/__init__.py
+++ command/__init__.py
@@ -1,5 +1,5 @@
 """command - Shell scripts for nursing a linux machine"""
 
-__version__ = '0.1.0'
-__author__ = 'chr1st1ank <chr1st1ank@pferdeplan.de>'
+__version__ = "0.1.0"
+__author__ = "chr1st1ank <chr1st1ank@pferdeplan.de>"
 __all__ = []
